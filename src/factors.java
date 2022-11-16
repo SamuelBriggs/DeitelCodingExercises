d46a042cc76f99998cefb159c6171fc976dbf0d3
Samuel Alawode <tolulope.alawode@gmail.com>
2022-11-16 03:37:26 -0800

    added new java files

---
+++ src/factors.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class factors {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int count = 0;
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+        int startingPoint = 1;
+
+        while (startingPoint <= number)
+            {
+                if (number % startingPoint == 0) {count += 1;}
+                startingPoint += 1;
+            }
+        System.out.println(count);
+    }
+}
+
+
